85b57384a02c99c025da3e8b917dd3f942c12a94
Inmo <inmo@MacBook-Pro-Inmo.local>
2021-01-09 18:49:15 +0200

    Добавлена страница каталога и адаптив, исправленны ошибки

---
+++ assets/img/Catalog_files/script.js
@@ -0,0 +1,15 @@
+// VARS
+const burgerBtn = document.querySelector('.nav_burger');
+const mainMenu = document.querySelector('.nav');
+
+
+const toogler = (item, itemClass) => {
+    item.classList.toggle(itemClass);
+}
+
+document.addEventListener('click', event => {
+    if (event.target.closest('.nav_burger')) {
+        toogler(burgerBtn, 'active');
+        toogler(mainMenu, 'active');
+    }
+})
