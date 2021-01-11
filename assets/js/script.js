fb280798636a05a1c0d2a74def649c369bff2b1d
Inmo <inmo@MacBook-Pro-Inmo.local>
2021-01-11 18:38:08 +0200

    Функции формы поиска, адаптив формы поиска

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // VARS
 const burgerBtn = document.querySelector('.nav_burger');
 const mainMenu = document.querySelector('.nav');
+const searchIcon = document.querySelector('.nav-btn__item--search');
+const searchForm = document.querySelector('.search-form');
+const searchInput = document.querySelector('.search-form__input');
 const shareIcon = document.querySelector('.nav-btn__item--share');
 const footerShareIcon = document.querySelector('.footer__icon--share');
 const shareTitle = document.querySelector('title').textContent;
@@ -18,14 +21,31 @@ if (navigator.share) {
 
 // CLICK EVENT
 document.addEventListener('click', event => {
+
     // MENU TOGGLE BUTTON
     if (event.target.closest('.nav_burger')) {
         toogler(burgerBtn, 'active');
         toogler(mainMenu, 'active');
         toogler(document.body, 'disabled');
     }
+
+    // SEARCH FORM
+    if (event.target === searchIcon) {
+        toogler(searchForm, 'visible');
+        searchInput.removeAttribute('disabled', '');
+        searchInput.focus();
+    }
+
+    // SHOW/HIDE SEARCH FORM
+    if (searchForm.classList.contains('visible') &&
+        !event.target.closest('.search-form') &&
+        !event.target.classList.contains('nav-btn__item--search')) {
+        searchForm.classList.remove('visible');
+        searchInput.blur();
+    }
+
     // SHARE BUTTON ACTION
-    if (event.target == shareIcon || event.target == footerShareIcon) {
+    if (event.target === shareIcon || event.target === footerShareIcon) {
         shareData = {
             title: shareTitle,
             url: location.href
